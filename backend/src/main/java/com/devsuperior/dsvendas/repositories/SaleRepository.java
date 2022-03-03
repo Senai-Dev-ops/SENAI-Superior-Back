66c4cb4a02161ed6b4ea695f2ddd26333e17d451
diogolimalucasdev <diogolimalucas.dev@gmail.com>
2022-03-03 15:20:23 -0300

    Repository Sale

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsvendas.entities.Seller;
 
-public interface SellerRepository extends JpaRepository<Seller, Long> {
+public interface SaleRepository extends JpaRepository<Seller, Long> {
+	
+	
 
 }
