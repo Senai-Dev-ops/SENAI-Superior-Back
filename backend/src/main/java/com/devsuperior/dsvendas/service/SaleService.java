21f77661b774b5d7f83fecdcb7da0ce0e4a7cc8f
GabrielSF <gabrielsantos.gsf@gmail.com>
2022-03-03 16:12:59 -0300

    Ajuste SaleService

--- backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
@@ -16,7 +17,10 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
+	
+	@Transactional(readOnly = true)
 	public List<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
